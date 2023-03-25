2789ded8b30179d4287a79926cdf2dde29410ca7
BolzmanBrain <antony.bricks6@yandex.ru>
2023-03-25 12:33:08 +0300

    Homework #13 - Deleted tests :(

--- homework13/src/main/java/ru/otus/spring/security/SecurityConfiguration.java
+++ homework13/src/main/java/ru/otus/spring/security/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package ru.otus.spring.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,12 +8,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
     public static final String LOGIN_URL = "/login";
-    public static final String USER_ROLE = "user";
-    public static final String SUPERUSER_ROLE = "superuser";
+
     @Bean
     public SecurityFilterChain securityFilterChain( HttpSecurity http ) throws Exception {
         http
@@ -29,12 +26,12 @@ public class SecurityConfiguration {
                                 "/api/v1/authors",
                                 "/api/v1/books/**").permitAll()
                         .antMatchers("/edit_book",
-                                "/add_book").hasAnyRole(USER_ROLE, SUPERUSER_ROLE)
+                                "/add_book").hasAnyRole(Roles.USER_ROLE, Roles.SUPERUSER_ROLE)
                         .antMatchers(HttpMethod.POST,
-                                "/api/v1/books").hasAnyRole(USER_ROLE, SUPERUSER_ROLE)
+                                "/api/v1/books").hasAnyRole(Roles.USER_ROLE, Roles.SUPERUSER_ROLE)
                         .antMatchers(HttpMethod.PUT,
-                                "/api/v1/books").hasAnyRole(USER_ROLE, SUPERUSER_ROLE)
-                        .anyRequest().hasRole(SUPERUSER_ROLE)
+                                "/api/v1/books").hasAnyRole(Roles.USER_ROLE, Roles.SUPERUSER_ROLE)
+                        .anyRequest().hasRole(Roles.SUPERUSER_ROLE)
                 )
                 .formLogin()
                 .loginProcessingUrl(LOGIN_URL)
